acc874183bf067c1311039e76f8def22b15be868
asd741 <a22833593@gmail.com>
2018-11-04 21:42:14 +0800

    practice

--- todomvc-app-template/components/header.jsx
+++ todomvc-app-template/components/header.jsx
@@ -1,16 +1,21 @@
 class Header extends React.Component {
-    constructor(props){
-        super()
-        this.state={
-            addDate:props.keyDown
-        }
-    }
-    render() {
-        return (
-            <header className="header">
-                <h1>todos</h1>
-                <input onKeyDown={this.state.addDate}className="new-todo" placeholder="What needs to be done?" autoFocus />
-            </header>
-        )
-    }
+	constructor(props) {
+		super();
+		this.state = {
+			addDate: props.keyDown
+		};
+	}
+	render() {
+		return (
+			<header className="header">
+				<h1>todos</h1>
+				<input
+					onKeyDown={this.state.addDate}
+					className="new-todo"
+					placeholder="What needs to be done?"
+					autoFocus
+				/>
+			</header>
+		);
+	}
 }
